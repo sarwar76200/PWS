9cc69c8020263721588cc73fb11bb449d8c0ba68
mohaimenul-haque <127889712+mohaimenul-haque@users.noreply.github.com>
2023-05-15 02:10:59 +0600

    modify homepage

--- homepage.php
+++ homepage.php
@@ -8,6 +8,15 @@
     <title>Home Page</title>
 
     <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" />
+    
+    <link rel="stylesheet" href="css/jquery.cleditor.css">
+</head>
+<body>
+    <script src="app.js"></script>
+</body>
+</html>
+
 </head>
 
 <body>
@@ -18,7 +27,7 @@
         <a href="ViewAllPrescription.php">View All Prescription</a>
         <a href="drugsDatabase.php">Drug Database</a>
         <a href="sms.php">SMS</a>
-        <a href="appointment.php">Appointment</a>
+        <!-- <a href="appointment.php">Appointment</a> -->
         <div class="animation start-home"></div>
     </nav>
 
@@ -203,10 +212,12 @@
             </div>
 
 
+
+
             <div class="mainBar">
 
-                <div>
-                    <table>
+                <div >
+                    <table class="med" >
                         <tr>
                             <th>No.</th>
                             <th>Brand</th>
@@ -214,46 +225,120 @@
                             <th>Instruction</th>
                             <th>Duration</th>
                         </tr>
-                        <tr class="mainBarTr">
-                            <td><input type="text" name="no1" class="no" id="no1" value="1"></td>
-                            <td>
-                                <input type="text" name="brand1" class="brand" id="brand1" placeholder="Medicine........." autocomplete="off">
-                                <div class="MedicineList" id="MedicineList"></div>
-                            </td>                            
-                            <td>
-                                <input type="text" name="dose1" class="Dose" id="dose1" placeholder="Dose........." autocomplete="off">
-                                <div class="DoseList" id="DoseList"></div>
-                            </td>                            
-                            <td>
-                                <input type="text" name="instruction1" class="Instruction" id="instruction1" placeholder="Instruction........." autocomplete="off">
-                                <div class="InstructionList" id="InstructionList"></div>
-                            </td>                            
-                            <td>
-                                <input type="text" name="duration1" class="Duration" id="duration1" placeholder="Duration........." autocomplete="off">
-                                <div class="DurationList" id="DurationList"></div>
-                            </td>                            
-                        </tr>
+
+                        <tbody id="table_body">
+
+                            <tr class="mainBarTr" >
+                            
+                                <td><input type="text" name="no1" class="no" id="no1" value="1"></td>
+                                <td>
+                                    <input type="text" name="brand1" class="brand" id="brand1" placeholder="Medicine........." autocomplete="off">
+                                    <div class="MedicineList" id="MedicineList"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="dose1" class="Dose" id="dose1" placeholder="Dose........." autocomplete="off">
+                                    <div class="DoseList" id="DoseList"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="instruction1" class="Instruction" id="instruction1" placeholder="Instruction........." autocomplete="off">
+                                    <div class="InstructionList" id="InstructionList"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="duration1" class="Duration" id="duration1" placeholder="Duration........." autocomplete="off">
+                                    <div class="DurationList" id="DurationList"></div>
+                                </td>
+                      
+                            </tr>
+                        
+
+                            <tr class="mainBarTr">
+                                <td><input type="text" name="no2" class="no" id="no2" value="2"></td>
+                                <td>
+                                    <input type="text" name="brand2" class="brand" id="brand2" placeholder="Medicine........." autocomplete="off">
+                                    <div class="MedicineList" id="MedicineList2"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="dose2" class="Dose" id="dose2" placeholder="Dose........." autocomplete="off">
+                                    <div class="DoseList" id="DoseList2"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="instruction2" class="Instruction" id="instruction2" placeholder="Instruction........." autocomplete="off">
+                                    <div class="InstructionList" id="InstructionList2"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="duration2" class="Duration" id="duration2" placeholder="Duration........." autocomplete="off">
+                                    <div class="DurationList" id="DurationList2"></div>
+                                </td>                            
+                            </tr>
 
 
-                        <tr class="mainBarTr">
-                            <td><input type="text" name="no1" class="no" id="no1" value="2"></td>
-                            <td>
-                                <input type="text" name="brand1" class="brand" id="brand1" placeholder="Medicine........." autocomplete="off">
-                                <div class="MedicineList" id="MedicineList"></div>
-                            </td>                            
-                            <td>
-                                <input type="text" name="dose1" class="Dose" id="dose1" placeholder="Dose........." autocomplete="off">
-                                <div class="DoseList" id="DoseList"></div>
-                            </td>                            
-                            <td>
-                                <input type="text" name="instruction1" class="Instruction" id="instruction1" placeholder="Instruction........." autocomplete="off">
-                                <div class="InstructionList" id="InstructionList"></div>
-                            </td>                            
-                            <td>
-                                <input type="text" name="duration1" class="Duration" id="duration1" placeholder="Duration........." autocomplete="off">
-                                <div class="DurationList" id="DurationList"></div>
-                            </td>                            
-                        </tr>
+
+                            <tr class="mainBarTr">
+                                <td><input type="text" name="no3" class="no" id="no3" value="3"></td>
+                                <td>
+                                    <input type="text" name="brand3" class="brand" id="brand3" placeholder="Medicine........." autocomplete="off">
+                                    <div class="MedicineList" id="MedicineList3"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="dose3" class="Dose" id="dose3" placeholder="Dose........." autocomplete="off">
+                                    <div class="DoseList" id="DoseList3"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="instruction3" class="Instruction" id="instruction3" placeholder="Instruction........." autocomplete="off">
+                                    <div class="InstructionList" id="InstructionList3"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="duration3" class="Duration" id="duration3" placeholder="Duration........." autocomplete="off">
+                                    <div class="DurationList" id="DurationList3"></div>
+                                </td>                            
+                            </tr>
+
+
+
+                            <tr class="mainBarTr">
+                                <td><input type="text" name="no4" class="no" id="no4" value="4"></td>
+                                <td>
+                                    <input type="text" name="brand4" class="brand" id="brand4" placeholder="Medicine........." autocomplete="off">
+                                    <div class="MedicineList" id="MedicineList4"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="dose4" class="Dose" id="dose4" placeholder="Dose........." autocomplete="off">
+                                    <div class="DoseList" id="DoseList4"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="instruction4" class="Instruction" id="instruction4" placeholder="Instruction........." autocomplete="off">
+                                    <div class="InstructionList" id="InstructionList4"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="duration4" class="Duration" id="duration4" placeholder="Duration........." autocomplete="off">
+                                    <div class="DurationList" id="DurationList4"></div>
+                                </td>                            
+                            </tr>
+
+
+
+                            <tr class="mainBarTr">
+                                <td><input type="text" name="no5" class="no" id="no5" value="5"></td>
+                                <td>
+                                    <input type="text" name="brand5" class="brand" id="brand5" placeholder="Medicine........." autocomplete="off">
+                                    <div class="MedicineList" id="MedicineList5"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="dose5" class="Dose" id="dose5" placeholder="Dose........." autocomplete="off">
+                                    <div class="DoseList" id="DoseList5"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="instruction5" class="Instruction" id="instruction5" placeholder="Instruction........." autocomplete="off">
+                                    <div class="InstructionList" id="InstructionList5"></div>
+                                </td>                            
+                                <td>
+                                    <input type="text" name="duration5" class="Duration" id="duration5" placeholder="Duration........." autocomplete="off">
+                                    <div class="DurationList" id="DurationList5"></div>
+                                </td>                            
+                            </tr>
+
+
+                        </tbody>
                         
 
                         
@@ -261,7 +346,22 @@
                         
 
                     </table>
+
+                    <button type="button" class="btn btn-success btn-sm" onclick="addEntry('table_body');">
+					    <span class="fa fa-plus"></span>
+				    </button>
                 </div>
+
+
+                <div class="output">
+                    <textarea name="input" id="input" cols="30" rows="10">
+                        <?php 
+                            // echo $Comment.'<br>';
+                        ?>
+                    </textarea>
+                </div>
+
+
 
             </div>
 
@@ -271,9 +371,66 @@
     </form>
 
 
+    
+
+
 
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/homepage.js"></script>
+
+    <script type="text/javascript" src="js/addRow.js"></script>
+
+
+    <!-- from here this is the notepad code -->
+
+    <script>
+        if ( window.history.replaceState ) 
+            {
+                window.history.replaceState( null, null, window.location.href );
+            }
+    </script>
+
+    <script src="js/jquery-3.6.4.min.js"></script>
+    <script src="js/jquery.cleditor.min.js"></script>
+    
+    <script>
+      $(document).ready(function() {
+          $("#input").cleditor({
+              width: 800, // width not including margins, borders or padding
+              height: 400, // height not including margins, borders or padding
+              controls: // controls to add to the toolbar
+                  "bold italic underline strikethrough subscript superscript | font size " +
+                  "style | color highlight removeformat | bullets numbering | outdent " +
+                  "indent | alignleft center alignright justify | undo redo | " +
+                  "rule image link unlink | cut copy paste pastetext | print source",
+              colors: // colors in the color popup
+                  "FFF FCC FC9 FF9 FFC 9F9 9FF CFF CCF FCF " +
+                  "CCC F66 F96 FF6 FF3 6F9 3FF 6FF 99F F9F " +
+                  "BBB F00 F90 FC6 FF0 3F3 6CC 3CF 66C C6C " +
+                  "999 C00 F60 FC3 FC0 3C0 0CC 36F 63F C3C " +
+                  "666 900 C60 C93 990 090 399 33F 60C 939 " +
+                  "333 600 930 963 660 060 366 009 339 636 " +
+                  "000 300 630 633 330 030 033 006 309 303",
+              fonts: // font names in the font popup
+                  "Arial,Arial Black,Comic Sans MS,Courier New,Narrow,Garamond," +
+                  "Georgia,Impact,Sans Serif,Serif,Tahoma,Trebuchet MS,Verdana," +
+                  "Siyam Rupali",
+              sizes: // sizes in the font size popup
+                  "1,2,3,4,5,6,7",
+              styles: // styles in the style popup
+                  [["Paragraph", "<p>"], ["Header 1", "<h1>"], ["Header 2", "<h2>"],
+                  ["Header 3", "<h3>"],  ["Header 4","<h4>"],  ["Header 5","<h5>"],
+                  ["Header 6","<h6>"]],
+              useCSS: false, // use CSS to style HTML when possible (not supported in ie)
+              docType: // Document type contained within the editor
+                  '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">',
+              docCSSFile: // CSS file used to style the document contained within the editor
+                  "",
+              bodyStyle: // style to assign to document body contained within the editor
+                  "margin:4px; font:10pt Arial,Verdana; cursor:text"
+          });
+      });
+  </script>
 
 
 </body>
