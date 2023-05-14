2142ebe37857f31a19b6905b6716528fe340bcfe
mohaimenul-haque <127889712+mohaimenul-haque@users.noreply.github.com>
2023-05-15 01:40:27 +0600

    add load duration

---
+++ loadDuration.php
@@ -0,0 +1,30 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password ="";
+    $dataBaseName = "pws"; //database name means table name 
+
+    //connect database with php 
+    $conn = mysqli_connect($host,$user,$password,$dataBaseName);
+
+    $search_term = $_POST['duration11'];
+
+    $sql = "SELECT distinct(duration_Type) FROM duration WHERE duration_Type LIKE '$search_term%'";
+    $result = mysqli_query($conn, $sql) or die("SQL Query Failed.");
+
+    $output = "<ul>";
+
+        if(mysqli_num_rows($result) > 0){  
+            while($row = mysqli_fetch_assoc($result)){
+                $output .= "<li>{$row['duration_Type']}</li>";
+            }
+    }else{  
+        $output .= "<li>Duration Not Found</li>";
+    } 
+    $output .= "</ul>";
+
+    echo $output;
+
+
+
+?>
