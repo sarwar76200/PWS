c8635fcd906bc38b2eab6c5d60bc9e0b5968bbef
mohaimenul-haque <127889712+mohaimenul-haque@users.noreply.github.com>
2023-05-24 19:47:56 +0600

    update homepage

--- homepage.php
+++ homepage.php
@@ -1,3 +1,152 @@
+<?php
+
+    error_reporting(0);
+    include("PHPConnect.php");
+
+    /**----------------------------------------------------------------------------------- 
+    * This Code is show the value in RegNO field and auto increment value after SUBMIT 
+    -------------------------------------------------------------------------------------*/
+    $query = "SELECT * FROM patientdatabase ORDER BY RegNo DESC LIMIT 1";
+    $result = mysqli_query($conn,$query);
+    $row = mysqli_fetch_array($result);
+    $last_RegNo = $row['RegNo'];
+
+    if ($last_RegNo == " ")
+    {
+        $patient_RegNo = 1; 
+    }
+    else
+    {
+        $patient_RegNo = ($last_RegNo + 1);   
+    }
+
+
+
+    if(isset($_POST['submit_btn'])){
+
+        $Name = $_POST['name'];
+        $Age = $_POST['age'];
+        $Sex = $_POST['sex'];
+        $Mobile = $_POST['mobile'];
+        $Reg = $_POST['reg'];
+        $Address = $_POST['address'];
+        $Date = $_POST['date'];
+
+
+        // echo 'Name : '.$Name.'<br>';
+        // echo $Age;
+        // echo $Sex;
+        // echo $Mobile;
+        // echo $Reg;
+        // echo $Address;
+        // echo $Date;
+
+
+        // $sql = "INSERT INTO patientdatabase(RegNo,Patient_Name,Age,Sex,Mobile,Address,Date) 
+        // VALUES('$Reg','$Name','$Age','$Sex','$Mobile','$Address','$Date')";
+
+        // $query1 = mysqli_query($conn,$sql);
+        // header("Refresh:0;");
+
+
+
+        $BP = $_POST['BP'];
+        $Pulse = $_POST['Pulse'];
+        $Temp = $_POST['Temp'];
+        $SpO2 = $_POST['SpO2'];
+        $Heart = $_POST['Heart'];
+        $Lungs = $_POST['Lungs'];
+        $Abd = $_POST['Abd'];
+        $Anaemia = $_POST['Anaemia'];
+        $Janudice = $_POST['Janudice'];
+        $Cyanosis = $_POST['Cyanosis'];
+        $Oedema = $_POST['Oedema'];
+
+
+        // echo $BP.'<br>';
+        // echo $Temp.'<br>';
+        // echo $Heart.'<br>';
+        // echo $Lungs.'<br>';
+
+
+        $ho = $_POST['H/O'];
+        $HO = "";
+
+        foreach($ho as $ho2){
+            $HO .= " *".$ho2;
+        }
+
+        // echo $HO;
+
+
+        $No1 = $_POST['no1'];
+        $Brand1 = $_POST['brand1'];
+        $Dose1 = $_POST['dose1'];
+        $Instruction1 = $_POST['instruction1'];
+        $Duration1 = $_POST['duration1'];
+
+        echo $No1;
+        echo $Brand1;
+        echo $Dose1;
+        echo $Instruction1;
+        echo $Duration1;
+        
+        
+        $No2 = $_POST['no2'];
+        $Brand2 = $_POST['brand2'];
+        $Dose2 = $_POST['dose2'];
+        $Instruction2 = $_POST['instruction2'];
+        $Duration2 = $_POST['duration2'];
+        
+
+        $No3 = $_POST['no3'];
+        $Brand3 = $_POST['brand3'];
+        $Dose3 = $_POST['dose3'];
+        $Instruction3 = $_POST['instruction3'];
+        $Duration3 = $_POST['duration3'];
+
+        $No4 = $_POST['no4'];
+        $Brand4 = $_POST['brand4'];
+        $Dose4 = $_POST['dose4'];
+        $Instruction4 = $_POST['instruction4'];
+        $Duration4 = $_POST['duration4'];
+    
+
+        $No5 = $_POST['no5'];
+        $Brand5 = $_POST['brand5'];
+        $Dose5 = $_POST['dose5'];
+        $Instruction5 = $_POST['instruction5'];
+        $Duration5 = $_POST['duration5'];
+        
+
+
+        // for($i=1;$i<10;$i++){
+        //     echo $No.$i." ".$Brand.$i." ".$Dose.$i." ".$Instruction.$i." ".$Duration.$i."<br>";
+        // }
+
+    }
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,7 +181,7 @@
     </nav>
 
 
-    <form action="">
+    <form action="" method="post">
         <div class="formHead">
 
             <label for="">Name &emsp;:</label>
@@ -55,7 +204,7 @@
             <input type="text" class="name" name="mobile">
 
             <label for="">Reg No.:</label>
-            <input type="text" class="age" name="reg">
+            <input type="text" class="age" name="reg" id="Reg" value="<?php echo $patient_RegNo ?>" readonly>
 
             <label for="">Address&ensp;:</label>
             <input type="textarea" class="address" name="address">
@@ -70,7 +219,7 @@
         </div>
 
         <div class="button">
-            <input type="submit" value="Submit" class="submit">
+            <input type="submit" value="Submit" class="submit" name="submit_btn" >
         </div>
 
         <div class="mainpart">
@@ -89,7 +238,7 @@
                         </thead>
                         <tr>
                             <td class="oe_col">BP</td>
-                            <td class="value_col"><textarea name="BP" class="value" id="" cols="0" rows="1"></textarea></td>
+                            <td class="value_col"><textarea name="BP" class="value" id="" cols="0" rows="1">120/80</textarea></td>
                             <td class="unit_col">mmHg</td>
                         </tr>
 
@@ -101,31 +250,31 @@
 
                         <tr>
                             <td class="oe_col">Temp</td>
-                            <td class="value_col"><textarea name="Temp" class="value" id="" cols="0" rows="1"></textarea></td>
+                            <td class="value_col"><textarea name="Temp" class="value" id="" cols="0" rows="1">98.6</textarea></td>
                             <td class="unit_col"><sup>o</sup>F</td>
                         </tr>
 
                         <tr>
-                            <td class="oe_col">SpO2</td>
+                            <td class="oe_col">SpO<sub>2</sub></td>
                             <td class="value_col"><textarea name="SpO2" class="value" id="" cols="0" rows="1"></textarea></td>
                             <td class="unit_col">%</td>
                         </tr>
 
                         <tr>
                             <td class="oe_col">Heart</td>
-                            <td class="value_col"><textarea name="Heart" class="value" id="" cols="0" rows="1"></textarea></td>
+                            <td class="value_col"><textarea name="Heart" class="value" id="" cols="0" rows="1">S1+S2+M0</textarea></td>
                             <td class="unit_col"></td>
                         </tr>
 
                         <tr>
                             <td class="oe_col">Lungs</td>
-                            <td class="value_col"><textarea name="Lungs" class="value" id="" cols="0" rows="1"></textarea></td>
+                            <td class="value_col"><textarea name="Lungs" class="value" id="" cols="0" rows="1">NAD</textarea></td>
                             <td class="unit_col"></td>
                         </tr>
 
                         <tr>
                             <td class="oe_col">Abd</td>
-                            <td class="value_col"><textarea name="Abd" class="value" id="" cols="0" rows="1"></textarea></td>
+                            <td class="value_col"><textarea name="Abd" class="value" id="" cols="0" rows="1">Soft, Non-Tender</textarea></td>
                             <td class="unit_col"></td>
                         </tr>
 
@@ -168,40 +317,36 @@
                         </thead>
                         <tbody class="OHTableBody">
                             <tr>
-                                <td><input class="checkbox" type="checkbox" name="htn" value="HTN">HTN</td>
-                                <td><input class="checkbox" type="checkbox" name="dm" value="DM">DM</td>
-                                <td><input class="checkbox" type="checkbox" name="asthma" value="Asthma">Asthma</td>
+                                <td><input class="checkbox" type="checkbox" name="H/O[]" value="HTN">HTN</td>
+                                <td><input class="checkbox" type="checkbox" name="H/O[]" value="DM">DM</td>
+                                <td><input class="checkbox" type="checkbox" name="H/O[]" value="Asthma">Asthma</td>
                             </tr>
 
                             <tr>
-                                <td><input class="checkbox" type="checkbox" name="copd" value="COPD">COPD</td>
-                                <td><input class="checkbox" type="checkbox" name="ihd" value="IHD">IHD</td>
-                                <td><input class="checkbox" type="checkbox" name="ckd" value="CKD">CKD</td>
+                                <td><input class="checkbox" type="checkbox" name="H/O[]" value="COPD">COPD</td>
+                                <td><input class="checkbox" type="checkbox" name="H/O[]" value="IHD">IHD</td>
+                                <td><input class="checkbox" type="checkbox" name="H/O[]" value="CKD">CKD</td>
                             </tr>
 
                             <tr>
-                                <td><input class="checkbox" type="checkbox" name="cld" value="CLD">CLD</td>
-                                <td><input class="checkbox" type="checkbox" name="cvd" value="CVD">CVD</td>
-                                <td><input class="checkbox" type="checkbox" name="smoking" value="Smoking">Smoking</td>
+                                <td><input class="checkbox" type="checkbox" name="H/O[]" value="CLD">CLD</td>
+                                <td><input class="checkbox" type="checkbox" name="H/O[]" value="CVD">CVD</td>
+                                <td><input class="checkbox" type="checkbox" name="H/O[]" value="Smoking">Smoking</td>
                             </tr>
 
                             <tr>
-                                <td><input class="checkbox" type="checkbox" name="tobacco_chewing" value="Tobacco Chewing" aria-colindex="2">Tobacco Chewing</td>
-                                <td></td>
-                                <td><input class="checkbox" type="checkbox" name="malignancy" value="Malignancy">Malignancy</td>
+                                <td colspan="2"><input class="checkbox" type="checkbox" name="H/O[]" value="Tobacco Chewing" aria-colindex="2">Tobacco Chewing</td>
+                                <td><input class="checkbox" type="checkbox" name="H/O[]" value="Malignancy">Malignancy</td>
                             </tr>
 
                             <tr>
-                                <td><input class="checkbox" type="checkbox" name="psychiatric_disorder" value="Psychiatric Disorder">Psychiatric Disorder</td>
-                                <td></td>
-                                <td><input class="checkbox" type="checkbox" name="allergy" value="Allergy">Allergy</td>
+                                <td colspan="2"><input class="checkbox" type="checkbox" name="H/O[]" value="Psychiatric Disorder">Psychiatric Disorder</td>
+                                <td><input class="checkbox" type="checkbox" name="H/O[]" value="Allergy">Allergy</td>
                             </tr>
 
                             <tr>
-                                <td><input class="checkbox" type="checkbox" name="drug_abuse" value="Drug Abuse">Drug Abuse</td>
-                                <td></td>
-                                </td>
-                                <td><input class="checkbox" type="checkbox" name="depression" value="Depression">Depression</td>
+                                <td colspan="2"><input class="checkbox" type="checkbox" name="H/O[]" value="Drug Abuse">Drug Abuse</td>
+                                <td><input class="checkbox" type="checkbox" name="H/O[]" value="Depression">Depression</td>
                             </tr>
 
                         </tbody>
@@ -226,6 +371,7 @@
                             <th>Duration</th>
                         </tr>
 
+                        
                         <tbody id="table_body">
 
                             <tr class="mainBarTr" >
